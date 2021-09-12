a9c1def6db888c0500106d34413e9d10eb9b2b32
MN-SHABNAM-MARIAM <shabnammariam@gmail.com>
2021-09-12 23:52:19 +0530

    made alterations

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 
 
-const slides = document.getElementsByClassName("carousel-item");
-let slidePosition = 0;
-const totalSlides = slides.length;
+const slides = document.getElementsByClassName("carousel-item")
+let slidePosition = 0
+const totalSlides = slides.length
 
 function moveToNextSlide() {
     slides[slidePosition].classList.remove("carousel-item-visible")
     if (slidePosition === totalSlides-1){
-        slidePosition = 0;
+        slidePosition = 0
     } else{
-        slidePosition++;
+        slidePosition++
     }
     slides[slidePosition].classList.add("carousel-item-visible")
     
@@ -17,10 +17,17 @@ function moveToNextSlide() {
 
 
 function moveToPrevSlide() {
-    
+    slides[slidePosition].classList.remove("carousel-item-visible")
+    if (slidePosition === 0){
+        slidePosition = totalSlides-1
+    } else{
+        slidePosition--
+    }
+    slides[slidePosition].classList.add("carousel-item-visible")
+   
 }
 
-document.getElementById("carousel-button-next").addEventListener("click", moveToNextSlide);
+document.getElementById("carousel-button-next").addEventListener("click", moveToNextSlide)
 document.getElementById("carousel-button-prev").addEventListener("click", moveToPrevSlide)
 
-console.log(totalSlides);
+console.log(totalSlides)
